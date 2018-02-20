de5669ce290d94bd75c13867b9e02e63330060ff
B <pintu.b@sap.com>
2018-02-20 22:40:50 +0530

    Committing intial working Build

---
+++ app/src/main/java/com/p2/android/batteryplus/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.p2.android.batteryplus
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
